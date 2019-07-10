322d60f316c1f49e668ea8eea172162594f9e272
Joe <neferry2010@gmail.com>
2019-07-10 20:06:06 +0800

    初始化

---
+++ 01/services/QueryAdressFromIP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _01.services
+{
+    public class QueryAdressFromIP
+
+    {
+        private long IPToNumber(string ip)
+        {
+            try
+            {
+                char[] separator = new char[] { '.' };//定义分割数组
+                string[] items = ip.Split(separator);//分割数组
+                return (  long.Parse(items[0]) << 24 
+                        | long.Parse(items[1]) <<16
+                        | long.Parse(items[2])  <<8
+                        | long.Parse(items[3]) );
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
